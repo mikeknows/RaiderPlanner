0e852a7f5fff0646e9bf855e02fa0bf4e3a30210
plymi <plymi@DESKTOP-87RMPED>
2019-09-06 11:52:46 -0400

    branch example

--- src/edu/wright/cs/raiderplanner/view/Main.java
+++ src/edu/wright/cs/raiderplanner/view/Main.java
@@ -37,6 +37,7 @@ public class Main extends Application {
 		// If initialization passed, call the Main menu:
 		MainController.main();
 		// UiManager.areYouFeelingLucky();
+		System.out.println("ok");
 	}
 
 	/**
